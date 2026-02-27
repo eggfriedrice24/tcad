17e27c3b09cd69ee4520cd511a9ee77944e05288
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-02-27 17:08:05 +0000

    remove ToolProvider and WorkspaceProvider from app shell

--- src/app.tsx
+++ src/app.tsx
@@ -4,29 +4,23 @@ import { StatusBar } from "@/components/layout/status-bar";
 import { ToolSidebar } from "@/components/layout/tool-sidebar";
 import { Toolbar } from "@/components/layout/toolbar";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { ToolProvider } from "@/components/providers/tool-provider";
-import { WorkspaceProvider } from "@/components/providers/workspace-provider";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="system">
-      <ToolProvider>
-        <WorkspaceProvider>
-          <TooltipProvider>
-            <SidebarProvider>
-              <ToolSidebar />
-              <SidebarInset>
-                <Toolbar />
-                <CenterWorkspace />
-                <StatusBar />
-              </SidebarInset>
-              <PropertiesSidebar />
-            </SidebarProvider>
-          </TooltipProvider>
-        </WorkspaceProvider>
-      </ToolProvider>
+      <TooltipProvider>
+        <SidebarProvider>
+          <ToolSidebar />
+          <SidebarInset>
+            <Toolbar />
+            <CenterWorkspace />
+            <StatusBar />
+          </SidebarInset>
+          <PropertiesSidebar />
+        </SidebarProvider>
+      </TooltipProvider>
     </ThemeProvider>
   );
 }
